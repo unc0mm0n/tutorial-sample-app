5306d6279355aab3509ba95b5adc0f2395f3cde3
Yuval <yvw.bor@gmail.com>
2014-06-01 19:09:05 +0300

    Refracture specs

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,6 +8,6 @@ describe "User Pages" do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it { should have_title(full_title('Sign Up')) }
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,1 @@
-# Returns the full title on a per-page basis
-  BASE_TITLE = "Ruby on Rails Tutorial"
-
-  def full_title(page_title)
-    if !page_title.empty?
-      "#{BASE_TITLE} | #{page_title}"
-    else
-      BASE_TITLE
-    end
-  end
+include ApplicationHelper

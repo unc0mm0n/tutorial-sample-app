45738e73a270d359f3bf4aeb54668820383f86fb
Yuval <yvw.bor@gmail.com>
2014-06-04 22:56:41 +0300

    Add signup messages testing

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,6 +16,14 @@ describe "User Pages" do
       it "should not create a user" do
         expect { click_button "Create my account" }.not_to change(User, :count)
       end
+
+      describe "after invalid submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign Up') }
+        it { should have_content('error') }
+        it { should have_content("can't be blank") }
+      end
     end
 
     describe "with valid information" do
@@ -28,6 +36,14 @@ describe "User Pages" do
 
       it "should create a user" do
         expect { click_button "Create my account" }.to change(User, :count).by(1)
+      end
+
+      describe "after valid submission" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end

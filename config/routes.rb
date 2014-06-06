21421b24a5969ee26d8fa26d9cd7f33e8c613ffa
Yuval <yvw.bor@gmail.com>
2014-06-06 17:09:36 +0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 TutorialSampleApp::Application.routes.draw do
 
-  resources :users
+  resources :users, except: [:new]
+  resources :sessions, only: [:create]
+
   root 'static_pages#home'
+
   get 'help',    to: "static_pages#help"
   get 'about',   to: "static_pages#about"
   get 'contact', to: "static_pages#contact"
   get 'signup',  to: "users#new"
+  get 'signin',  to: "sessions#new"
+  delete 'signout',  to: "sessions#destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com", 
+  before { @user = User.new(name: "Example User", email: "user@example.com",
   								          password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
@@ -103,5 +103,10 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
